eabc8eea077ba55383e17b6b40d8513fa03ebf5e
Tintin <xiaoyu97@outlook.com>
2023-11-17 09:59:50 +0800

    chang lang

--- src/lang/index.js
+++ src/lang/index.js
@@ -1,34 +1,34 @@
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import Cookies from 'js-cookie'
-import elementEnLocale from 'element-ui/lib/locale/lang/en' // element-ui lang
+// import elementEnLocale from 'element-ui/lib/locale/lang/en' // element-ui lang
 import elementZhLocale from 'element-ui/lib/locale/lang/zh-CN'// element-ui lang
-import elementEsLocale from 'element-ui/lib/locale/lang/es'// element-ui lang
-import elementJaLocale from 'element-ui/lib/locale/lang/ja'// element-ui lang
-import enLocale from './en'
+// import elementEsLocale from 'element-ui/lib/locale/lang/es'// element-ui lang
+// import elementJaLocale from 'element-ui/lib/locale/lang/ja'// element-ui lang
+// import enLocale from './en'
 import zhLocale from './zh'
-import esLocale from './es'
-import jaLocale from './ja'
+// import esLocale from './es'
+// import jaLocale from './ja'
 
 Vue.use(VueI18n)
 
 const messages = {
-  en: {
-    ...enLocale,
-    ...elementEnLocale
-  },
+  // en: {
+  //   ...enLocale,
+  //   ...elementEnLocale
+  // },
   zh: {
     ...zhLocale,
     ...elementZhLocale
-  },
-  es: {
-    ...esLocale,
-    ...elementEsLocale
-  },
-  ja: {
-    ...jaLocale,
-    ...elementJaLocale
   }
+  // es: {
+  //   ...esLocale,
+  //   ...elementEsLocale
+  // },
+  // ja: {
+  //   ...jaLocale,
+  //   ...elementJaLocale
+  // }
 }
 export function getLanguage() {
   const chooseLanguage = Cookies.get('language')
